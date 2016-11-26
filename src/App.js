26d232c2cad87dde479c30045aa74cf9e8e2cb11
Greg Wiley <aztec.rex@jammm.com>
2016-11-25 20:39:38 -0800

    function style App component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import CommitView from './CommitView';
 import logo from './logo.svg';
 import './App.css';
@@ -13,21 +13,19 @@ const state = [
     }
 ];
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <CommitView commits={state}/>
+const App = () => {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React</h2>
       </div>
-    );
-  }
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+      <CommitView commits={state}/>
+    </div>
+  );
 }
 
 export default App;
